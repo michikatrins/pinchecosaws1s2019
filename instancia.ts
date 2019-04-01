e2e4d4392cf210281babb67d8c5eed744e9df775
michikatrins <michikatrins@gmail.com>
2019-03-31 21:17:17 -0600

    Add files via upload

---
+++ instancia.ts
@@ -0,0 +1,17 @@
+AWSTemplateFormatVersion: "2010-09-09"
+Description: Este es un pinche ejemplo de agregarInstancias :D
+Resources:
+  MyEC2Instance:
+    Type: "AWS::EC2::Instance"
+    Properties: 
+      ImageId: "ami-0ff8a91507f77f867"
+      InstanceType: t2.micro
+      KeyName: testkey
+      BlockDeviceMappings:
+        -
+          DeviceName: /dev/sdm
+          Ebs:
+            VolumeType: io1
+            Iops: 200
+            DeleteOnTermination: false
+            VolumeSize: 20
